3c2ea8c9185823a878ce17340bb8764d0f6cca79
598998 <598998@stud.hvl.no>
2021-11-02 13:22:59 +0100

    Bilde ferdig

--- src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
@@ -1,31 +1,43 @@
 package no.hvl.dat100.jplab11.oppgave2;
 
 import no.hvl.dat100.jplab11.common.TODO;
+import no.hvl.dat100.jplab11.oppgave1.Innlegg;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
+	private String url;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+
+		super(id, bruker, dato, tekst);
+		this.url = url;
+
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+
+		super (id, bruker, dato, likes, tekst);
+		this.url = url;
+
 	}
 	
 	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+
+		return url;
 
 	}
 
 	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
+
+		this.url = url;
+
 	}
 
 	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+	public String toString() { //BILDE\n1\nOle Olsen\n23-10\n0\net bilde\nhttp://www.picture.com/oo.jpg\n
+
+		return "BILDE" + "\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n"
+				+ getTekst() + "\n" + url + "\n";
 
 	}
 
