22dc3e5179176b7b1ece4eefdf3ca884987f13a0
598998 <598998@stud.hvl.no>
2021-11-02 15:15:29 +0100

    SkrivBlogg ferdig (tror den er riktig)

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,7 +1,10 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -10,6 +13,34 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		File file = new File(filnavn);
+		PrintWriter skriver = null;
+		Boolean utført = false;
+
+		try {
+
+			skriver = new PrintWriter(file);
+
+			if (samling.getAntall() > 0){
+
+				for (int i = 0; i < samling.getAntall(); i++){
+
+				skriver.print(samling.toString());
+
+				}
+
+				utført = true;
+
+			}
+
+		}
+
+		catch (FileNotFoundException e){
+
+		skriver.print("Fant ikke filen :(");
+
+		}
+
+		return utført;
 	}
 }
